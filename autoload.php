ed85555ba016d26b6f24f961a7ec6f871a393f1c
OpenShift guest <oliver@ananit.de>
2013-02-05 07:24:42 -0500

    bugfix: autoload failed if used as composer dependency

--- autoload.php
+++ autoload.php
@@ -1,5 +1,5 @@
 <?php
-foreach( array('/../autoload.php','/vendor/autoload.php') as $autoloader){
+foreach( array('/../../autoload.php','/vendor/autoload.php') as $autoloader){
     if( file_exists(__DIR__.$autoloader) ){ 
         require __DIR__.$autoloader; 
         break;
